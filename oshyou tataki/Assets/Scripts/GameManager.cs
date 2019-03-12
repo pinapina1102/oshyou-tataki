f3f47e11652299bd74c7997a2ad9e0bb02e4a9c0
PAD_of_MAMI <junkey.yama@gmail.com>
2019-03-12 15:07:30 +0900

    アニメーターのScript作成

--- oshyou tataki/Assets/Scripts/GameManager.cs
+++ oshyou tataki/Assets/Scripts/GameManager.cs
@@ -2,35 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour{
+public class GameManager : MonoBehaviour
+{
+    //定数定義
+    private const int LIMIT_TIME = 30;  //制限時間
 
     //オブジェクト参照
-    public GameObject osyou;        //和尚
-    public GameObject textScore;    //テキストスコア
-    public GameObject imageBonz;    //和尚のアニメーション
+    public GameObject Bonz;        //和尚
+    public GameObject textScore;   //テキストスコア
+    public GameObject imageBonz;  //和尚のアニメーション
+    public GameObject imageSmoke; //
 
     //メンバ変数
     private int score = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     //和尚をクリック
-    public void TouchBonz (){
-        if (Input.GetMouseButton (0) == false) {
+    public void TouchBonz()
+    {
+        if (Input.GetMouseButton(0) == false)
+        {
             return;
         }
-        Destroy(this.gameObject);
-    }
 
-    //和尚アニメ再生
-    AnimatorStateInfo stateInfo =
-        imageMokugyo.GetComponent<Animetor>().GetCurrentAnimatorStateInfo(0);
+        Destroy(this.gameObject);
+
+        //和尚アニメ再生
+        AnimatorStateInfo stateInfo =
+            imageBonz.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0);
+        if (stateInfo.fullPathHash ==
+                Animator.StringToHash("Base Layer.float@Bonz"))
+        {
+            //すでに再生中なら先頭から
+            imageBonz.GetComponent<Animator>().Play(stateInfo.fullPathHash, 0, 0.0f);
+        }
+        else
+        {
+            imageBonz.GetComponent<Animator>().SetTrigger("isGetScore");
+        }
+    }
+}
+
