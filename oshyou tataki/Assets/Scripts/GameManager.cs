6d375a07e5598d82a89866841fd587745b7d4fd1
PAD_of_MAMI <junkey.yama@gmail.com>
2019-03-12 12:11:40 +0900

    和尚アニメーション作成途中

--- oshyou tataki/Assets/Scripts/GameManager.cs
+++ oshyou tataki/Assets/Scripts/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : MonoBehaviour{
     //オブジェクト参照
     public GameObject osyou;        //和尚
     public GameObject textScore;    //テキストスコア
+    public GameObject imageBonz;    //和尚のアニメーション
 
     //メンバ変数
     private int score = 0;
@@ -23,10 +24,13 @@ public class GameManager : MonoBehaviour{
     }
 
     //和尚をクリック
-    public void TouchOshou (){
+    public void TouchBonz (){
         if (Input.GetMouseButton (0) == false) {
             return;
         }
         Destroy(this.gameObject);
     }
-}
+
+    //和尚アニメ再生
+    AnimatorStateInfo stateInfo =
+        imageMokugyo.GetComponent<Animetor>().GetCurrentAnimatorStateInfo(0);
