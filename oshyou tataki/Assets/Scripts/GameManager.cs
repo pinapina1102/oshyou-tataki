e3b6e711b4f0a95ed347167391c3b15316dbdbd9
PAD_of_MAMI <junkey.yama@gmail.com>
2019-03-28 16:07:23 +0900

    タイトル画面作成

--- oshyou tataki/Assets/Scripts/GameManager.cs
+++ oshyou tataki/Assets/Scripts/GameManager.cs
@@ -25,7 +25,7 @@ public class GameManager : MonoBehaviour
     private int score = 0;  //現在のスコア
     private int currentBonz = 0; //現在の和尚数
     private DateTime lastDateTime; //前回和尚を生成した時間
-    private Vector2[] x = new Vector2[9];
+    private Vector2[] position = new Vector2[9]; //シャッフルする配列
 
     // Start is called before the first frame update
     void Start()
@@ -57,6 +57,9 @@ public class GameManager : MonoBehaviour
             }
         }
     }
+
+    //
+
 
     //新しい和尚の生成
     public void CreateNewBonz()
