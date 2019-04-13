f0f509fc0fe60c813423b10b46205e2ce0c7a00d
PAD_of_MAMI <junkey.yama@gmail.com>
2019-04-13 11:48:19 +0900

    リザルトスクリプト作成

--- oshyou tataki/Assets/Scripts/ResultManager.cs
+++ oshyou tataki/Assets/Scripts/ResultManager.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ResultScene : MonoBehaviour
+using UnityEngine.UI;
+using UnityEditor.SceneManagement;
+
+public class ResultManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //ボタンを押したら0.1秒後タイトルへ
+        Invoke("GoBackTitleScene", 0.1f);
     }
+
+    //ステージシーンに戻る
+    void GoBackTitleScene(){
+        EditorSceneManager.LoadScene("TitleManager");
+    }
+
 }
